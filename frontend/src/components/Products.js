2093898d6269c0698fc6baf6e268671dabd8c60e
SahilPanhotra <sahilpanhotra@gmail.com>
2023-02-02 16:53:00 +0530

    build:add styled components and basic styling

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,21 +1,20 @@
 import Image from "next/image";
+import { ProductStyle } from "@/styles/ProductStyle";
 
 export default function Product({ product }) {
   const { title, price, image, Description } = product.attributes;
 
   return (
-    <div>
+    <ProductStyle>
       <div>
-        <Image
+        <img
           src={image.data.attributes.formats.small.url}
-          width={200}
-          height={200}
           alt={`${title}`}
         />
       </div>
       <h2>{title}</h2>
       <h3>{price}</h3>
       <p>{Description}</p>
-    </div>
+    </ProductStyle>
   );
 }
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { useQuery } from "urql";
 import { PRODUCT_QUERY } from "lib/query";
 import Product from "@/components/Products";
+import { Gallery } from "@/styles/Gallery";
 
 export default function Home() {
   const [results] = useQuery({ query: PRODUCT_QUERY });
@@ -9,7 +10,6 @@ export default function Home() {
   if (fetching) return <h1>Loading...</h1>;
   if (error) return <p>Oh no {error.message}</p>;
   const products = data.products.data;
-  console.log(products)
 
   return (
     <>
@@ -20,9 +20,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+      <Gallery>
         {products.map((product) => (
           <Product key={product.attributes.slug} product={product} />
         ))}
+        </Gallery>
       </main>
     </>
   );
