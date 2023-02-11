96f78542bad4f2b1270d71ade9cdd95dec79ff31
SahilPanhotra <sahilpanhotra@gmail.com>
2023-02-11 13:18:01 +0530

    feat:allow not loggedIn users to buy

--- frontend/src/pages/api/stripe.js
+++ frontend/src/pages/api/stripe.js
@@ -2,46 +2,80 @@ import Stripe from "stripe";
 const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
 import { getSession } from "@auth0/nextjs-auth0";
 
-
 export default async function handler(req, res) {
-  const session =await getSession(req,res);
-  console.log(session);
-  const user= session?.user;
-  console.log(user);
-  const stripeId=user[`${process.env.BASE_URL}/stripe_customer_id`];
+  const session = await getSession(req, res);
+  const user = session?.user;
 
-  if (req.method === "POST") {
-    try {
-      const session = await stripe.checkout.sessions.create({
-        submit_type: "pay",
-        mode: "payment",
-        customer:stripeId,
-        payment_method_types: ["card"],
-        allow_promotion_codes:true,
-        line_items: req.body.map((item) => {
-          return {
-            price_data: {
-              currency: "usd",
-              product_data: {
-                name: item.title,
-                images: [item.image.data.attributes.formats.thumbnail.url],
+  if (user) {
+    const stripeId = user[`${process.env.BASE_URL}/stripe_customer_id`];
+
+    if (req.method === "POST") {
+      try {
+        const session = await stripe.checkout.sessions.create({
+          submit_type: "pay",
+          mode: "payment",
+          customer: stripeId,
+          payment_method_types: ["card"],
+          allow_promotion_codes: true,
+          line_items: req.body.map((item) => {
+            return {
+              price_data: {
+                currency: "usd",
+                product_data: {
+                  name: item.title,
+                  images: [item.image.data.attributes.formats.thumbnail.url],
+                },
+                unit_amount: item.price * 100,
               },
-              unit_amount: item.price * 100,
-            },
-            adjustable_quantity:{
-                enabled:true,
-                minimum:1,  
-            },
-            quantity: item.quantity, 
-          };
-        }),
-        //bring people to success url
-        success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.origin}/canceled`,
-      });
-      res.status(200).json(session);
-    } catch (error) {
-      res.status(error.statusCode || 500).json(error.message);
+              adjustable_quantity: {
+                enabled: true,
+                minimum: 1,
+              },
+              quantity: item.quantity,
+            };
+          }),
+          //bring people to success url
+          success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${req.headers.origin}/canceled`,
+        });
+        res.status(200).json(session);
+      } catch (error) {
+        res.status(error.statusCode || 500).json(error.message);
+      }
+    }
+  } else {
+    if (req.method === "POST") {
+      try {
+        const session = await stripe.checkout.sessions.create({
+          submit_type: "pay",
+          mode: "payment",
+          payment_method_types: ["card"],
+          allow_promotion_codes: true,
+          line_items: req.body.map((item) => {
+            return {
+              price_data: {
+                currency: "usd",
+                product_data: {
+                  name: item.title,
+                  images: [item.image.data.attributes.formats.thumbnail.url],
+                },
+                unit_amount: item.price * 100,
+              },
+              adjustable_quantity: {
+                enabled: true,
+                minimum: 1,
+              },
+              quantity: item.quantity,
+            };
+          }),
+          //bring people to success url
+          success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${req.headers.origin}/canceled`,
+        });
+        res.status(200).json(session);
+      } catch (error) {
+        res.status(error.statusCode || 500).json(error.message);
+      }
     }
   }
 }
