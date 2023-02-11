5749d4d1dfc6749e2ad539e67169cf40f57751a8
SahilPanhotra <sahilpanhotra@gmail.com>
2023-02-11 13:44:11 +0530

    feat:Add a toast on Add item to Cart

--- frontend/src/pages/products/[slug].js
+++ frontend/src/pages/products/[slug].js
@@ -9,20 +9,28 @@ import {
 } from "@/styles/ProductDetails";
 import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
 import { useStateContext } from "lib/context";
-
+import { toast } from "react-hot-toast";
 
 export default function ProductDetail() {
-  const {qty,increaseQty,decreaseQty,onAdd} = useStateContext();
+  const { qty, increaseQty, decreaseQty, onAdd } = useStateContext();
   console.log(qty);
   const { query } = useRouter();
   const [results] = useQuery({
     query: GET_PRODUCT_QUERY,
     variables: { slug: query.slug },
   });
+
   const { data, fetching, error } = results;
   if (fetching) return <h1>Loading...</h1>;
   if (error) return <p>Oh no {error.message}</p>;
   const { title, Description, image } = data.products.data[0].attributes;
+
+  //create a toast
+  const notify = () => {
+    toast.success(`${title} Added to your Cart`,{duration:1500,
+    icon:"🛒"});
+  };
+  
   return (
     <DetailsStyle>
       <img src={image.data.attributes.formats.medium.url} alt={title} />
@@ -33,14 +41,22 @@ export default function ProductDetail() {
         <Quantity>
           <span>Quantity</span>
           <button>
-            <AiFillMinusCircle onClick={decreaseQty} />
+            <AiFillMinusCircle onClick={decreaseQty
+            } />
           </button>
           <p>{qty}</p>
           <button>
-            <AiFillPlusCircle  onClick={increaseQty}/>
+            <AiFillPlusCircle onClick={increaseQty} />
           </button>
         </Quantity>
-        <Buy onClick={()=>onAdd(data.products.data[0].attributes,qty)}>Add to Cart</Buy>
+        <Buy
+          onClick={() => {
+            onAdd(data.products.data[0].attributes, qty);
+            notify();
+          }}
+        >
+          Add to Cart
+        </Buy>
       </ProductInfo>
     </DetailsStyle>
   );
